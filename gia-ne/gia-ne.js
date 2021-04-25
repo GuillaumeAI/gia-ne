0e99e1359cd4fe1c3ad1b204fdfefed1c195ae44
Jean Guillaume <jgi@jgwill.com>
2021-04-25 01:14:30 -0400

    feat: Node CLI for NE Progressing

--- gia-ne/gia-ne.js
+++ gia-ne/gia-ne.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-/** Guillaume Isabelle GIA-NE
- * Vision: A Neural Enhancement for images encapsulated into a docker container and wrapped for multiplatform by nodeJS
+
+/** Guillaume Isabelle, 2021 (GIA-NE)
+ * Vision: A containerized Neural Image Enhancement wrapped for multiplatform by nodeJS
  
- * Current Reality:Copied from GIS-CSM
+ * Current Reality: 
  */
-var container_tag = "guillaumeai/gia-ne";
-var mount_in = "/work/input";
+var container_tag = "guillaumeai/ne:210419";
+var mount_in = "/ne/input";
 var mount_out = "/out";
 
 //@STCGoal  what comes next is up for GIA-NE
 //@STCSTatus A copy of GIS-CSM
+console.log("This programm will receive one argument and enhance the image or the whole folder if that is a folder.");
 
 
 var os = process.platform;
@@ -17,11 +19,27 @@ var os = process.platform;
 var myArgs = process.argv.slice(2);
 
 var target_file = myArgs[0];
+
+
+//----ZOOM FACTOR
+var zoomFactor = 2;
+if (myArgs[1]) zoomFactor= myArgs[1];
+var ne2xScript = "/ne/ne2x.sh";
+var ne4xScript = "/ne/ne4x.sh";
+var currentScript = ne2xScript;
+if (zoomFactor == 4) 
+  currentScript = ne4xScript;
+console.log("Zoom Factor: " + zoomFactor);
+
 var path = require('path');
+const { exit } = require('process');
 var resolve = path.resolve;
 var target_file_name_only = path.basename(target_file);
 var target_dir = path.dirname(target_file);
-// console.log(target_dir);
+console.log(target_dir);
+console.log(target_file);
+console.log(target_file_name_only);
+//process.exit(0);
 
 if (os == "win32") {
   //running context will use Powershell to run docker
@@ -75,12 +93,14 @@ function make_docker_cmd(output) {
     `${container_tag}  ` +
     `${target_file_name_only}`;
 
+    console.log(cmdToRun);
+
   platform_run(cmdToRun);
 
 }
 
 function platform_run(cmdToRun) {
-
+exit(1);
   console.log("Running: " + cmdToRun);
   console.log("  on platform: " + os);
 
